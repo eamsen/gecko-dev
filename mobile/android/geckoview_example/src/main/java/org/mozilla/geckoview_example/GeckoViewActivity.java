4dfa424729306feee7e4ba8fadbd6ff584d813e4
Eugen Sawin <esawin@mozilla.com>
2017-06-08 22:16:19 +0200

    Bug 1368701 - [2.1] Add shutdown intent action to shutdown the GeckoView example app. r=snorp

--- mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
+++ mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
@@ -15,11 +15,15 @@ import android.view.WindowManager;
 
 import org.mozilla.gecko.GeckoView;
 import org.mozilla.gecko.GeckoViewSettings;
+import org.mozilla.gecko.GeckoThread;
 
 public class GeckoViewActivity extends Activity {
     private static final String LOGTAG = "GeckoViewActivity";
     private static final String DEFAULT_URL = "https://mozilla.org";
     private static final String USE_MULTIPROCESS_EXTRA = "use_multiprocess";
+    private static final String ACTION_SHUTDOWN =
+        "org.mozilla.geckoview_example.SHUTDOWN";
+    private boolean mKillProcessOnDestroy;
 
     /* package */ static final int REQUEST_FILE_PICKER = 1;
 
@@ -50,8 +54,26 @@ public class GeckoViewActivity extends Activity {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mKillProcessOnDestroy) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+
+    @Override
     protected void onNewIntent(final Intent intent) {
         super.onNewIntent(intent);
+
+        if (ACTION_SHUTDOWN.equals(intent.getAction())) {
+            mKillProcessOnDestroy = true;
+            GeckoThread.forceQuit();
+            mGeckoView.onDetachedFromWindow();
+            finish();
+            return;
+        }
+
         setIntent(intent);
 
         if (intent.getData() != null) {
