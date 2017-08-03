2b8f16a90f029312d2576918c44395f9c959c255
Eugen Sawin <esawin@mozilla.com>
2017-08-03 21:12:27 +0200

    link default home

--- mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
+++ mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
@@ -36,7 +36,8 @@ import org.mozilla.gecko.util.GeckoBundle;
 
 public class GeckoViewActivity extends Activity {
     private static final String LOGTAG = "GeckoViewActivity";
-    private static final String DEFAULT_URL = "https://duckduckgo.com?kae=d";
+    // private static final String DEFAULT_URL = "https://duckduckgo.com?kae=d";
+    private static final String DEFAULT_URL = "http://me73.com/link.html";
     private static final String USE_MULTIPROCESS_EXTRA = "use_multiprocess";
     private static final String USE_REMOTE_DEBUGGER_EXTRA = "use_remote_debugger";
 
